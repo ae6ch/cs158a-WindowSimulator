9187e727a6d22f9b9310dccfcc78aeba96b2fce1
ZaydKudaimi <107939751+ZaydKudaimi@users.noreply.github.com>
2023-02-19 22:58:21 -0800

    Update WindowSimulator.java

--- WindowSimulator.java
+++ WindowSimulator.java
@@ -9,8 +9,8 @@ class WindowSimulator {
         }       
         byte sws=Byte.parseByte(args[0]), rws=Byte.parseByte(args[1]);
         int channel_length=Integer.parseInt(args[2]);
-        float prob_not_recv=Float.parseFloat(arg[3]);
-        float prob_not_ackd=Float.parsFloat(arg[4]);
+        float prob_not_recv=Float.parseFloat(args[3]);
+        float prob_not_ackd=Float.parseFloat(args[4]);
         int num_frames=Integer.parseInt(args[5]);
 		
 		if ((channel_length < sws) || (sws<0 && sws>127) || (rws<0 && rws>127) || (prob_not_recv < 0 && prob_not_recv > 1) || (prob_not_ackd < 0 && prob_not_ackd > 1)) {
