40636f3effbfbb0155128e17532f5ce6bec48ef9
Steve Rubin <ser@tch.org>
2023-02-20 18:54:37 -0800

    created Station class

---
+++ Station.java
@@ -0,0 +1,19 @@
+class Station {
+    Station(int sws, int rws, float propDrop) {
+    }
+    
+    boolean isReady() {
+
+    }
+    boolean send(int data) {
+
+    }
+
+    byte[] nextTransmitFrame() {
+
+    }
+
+    void receiveFrame(byte[] frame){
+
+    }
+}
